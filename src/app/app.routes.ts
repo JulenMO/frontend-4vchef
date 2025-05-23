aa71546bfc94af1989617f4d040b3fecd79cacde
julenmo <julen.medina.oses@gmail.com>
2025-05-24 00:51:49 +0200

    feat: Add recipe-creation screen and header navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,11 @@ export const routes: Routes = [
     {
         path: '',
         loadComponent: () =>
-            import('./components/recipe-list/recipe-list.component').then(
-                (m) => m.RecipeListComponent
-            ),
+            import('./components/recipe-list/recipe-list.component').then(m => m.RecipeListComponent)
     },
+    {
+        path: 'recipe-creation',
+        loadComponent: () =>
+            import('./components/recipe-creation/recipe-creation.component').then(m => m.RecipeCreationComponent)
+    }
 ];
---
+++ src/app/components/recipe-creation/recipe-creation.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-creation',
+  standalone: true,
+  templateUrl: './recipe-creation.component.html'
+})
+export class RecipeCreationComponent { }
